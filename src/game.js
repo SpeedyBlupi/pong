16cb1e36961a2f1448d882c8992f97b9f98e2e8f
Daniel Roux <roux@epsitec.ch>
2023-01-27 10:02:16 +0100

    Add best version with correct intersects.

--- src/game.js
+++ src/game.js
@@ -5,6 +5,7 @@ import Rect from "./rect";
 import Misc from "./misc";
 import Random from "./random";
 import Pong1 from "./pong1";
+import Pong2 from "./pong2";
 import PingPong1 from "./ping-pong1";
 import BlupiWalks1 from "./blupi-walks1";
 import BlupiWalks2 from "./blupi-walks2";
@@ -23,6 +24,7 @@ export default class Game {
     this.keysDown = new Map();
 
     this.pong1 = new Pong1();
+    this.pong2 = new Pong2();
     this.pingPong1 = new PingPong1();
     this.blupi1 = new BlupiWalks1();
     this.blupi2 = new BlupiWalks2();
@@ -58,6 +60,7 @@ export default class Game {
 
   step(device, elapsedTime, input) {
     this.pong1.step(device, elapsedTime, input);
+    this.pong2.step(device, elapsedTime, input);
     this.pingPong1.step(device, elapsedTime, input);
     this.blupi1.step(device, elapsedTime, input);
     this.blupi2.step(device, elapsedTime, input);
@@ -69,8 +72,9 @@ export default class Game {
     this.pixmap.drawIcon(device, "80x80", 7, area, 1, 0);
 
     // this.pong1.draw(device, this.pixmap);
+    this.pong2.draw(device, this.pixmap);
     // this.pingPong1.draw(device, this.pixmap);
-    this.blupi1.draw(device, this.pixmap);
-    this.blupi2.draw(device, this.pixmap);
+    // this.blupi1.draw(device, this.pixmap);
+    // this.blupi2.draw(device, this.pixmap);
   }
 }
